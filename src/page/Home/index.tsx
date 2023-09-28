a79a3f66c07b8c54b95ff8b0117f51bb30811821
bbbaipooo <baipoo_B@hotmail.com>
2023-09-29 01:31:18 +0700

    Baipoo check grammar

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -9,7 +9,7 @@ function Home() {
         <h1>Home page</h1>
         <h2 className='text-green-500'>chawong Check In</h2>
         <h2 className='text-rose-500'>chawong Check Out</h2>
-        <h2 className='text-rose-500'>baipoo Check Out but no checkIn</h2>
+        <h2 className='text-rose-500'>Baipoo /Checking Out Code -goto- Checking In Code/</h2>
         <div >
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
