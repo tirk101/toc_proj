1e0ea8edbd0ed0e10949eb39770cc9549a421632
Chanidapa Wongtep <ddreamc1@gmail.com>
2023-09-29 01:02:54 +0700

    baipoo checkout

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -7,7 +7,9 @@ function Home() {
   return (
       <div className=' flex flex-col justify-center items-center font-bold'>
         <h1>Home page</h1>
-        <h2 className='text-rose-500'>chawong CheckIn</h2>
+        <h2 className='text-green-500'>chawong Check In</h2>
+        <h2 className='text-rose-500'>chawong Check Out</h2>
+        <h2 className='text-rose-500'>baipoo Check Out but no checkIn</h2>
         <div >
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
