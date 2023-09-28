97e756202e4688606a0246b2c6b39c852a6f83b8
Natdanai <88823702+corodimas@users.noreply.github.com>
2023-09-29 02:44:02 +0700

    refactor

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -7,6 +7,9 @@ function Home() {
   return (
       <div className=' flex flex-col justify-center items-center font-bold'>
         <h1>Home page</h1>
+        <h2 className='text-green-500'>chawong Check In</h2>
+        <h2 className='text-rose-500'>chawong Check Out</h2>
+        <h2 className='text-rose-500'>Baipoo /Checking Out Code -goto- Checking In Code./</h2>
         <div >
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
