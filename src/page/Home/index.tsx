c7525f6a6eec149deeef013dceddf7980a6f0955
bbbaipooo <baipoo_B@hotmail.com>
2023-10-03 17:00:51 +0700

    new home

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,20 +1,13 @@
 import { useState } from 'react'
 import React from 'react'
+import {background} from "../../assets/home"
 
 function Home() {
   const [count, setCount] = useState(0)
 
   return (
-      <div className=' flex flex-col justify-center items-center font-bold'>
-        <h1>Home page</h1>
-        <h2 className='text-green-500'>chawong Check In</h2>
-        <h2 className='text-rose-500'>chawong Check Out</h2>
-        <h2 className='text-rose-500'>Baipoo /Checking Out Code -goto- Checking In Code./</h2>
-        <div >
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-        </div>
+    <div style={{ backgroundImage: `url(${background})` }} className='w-full h-[100vh] flex justify-center items-center gap-[5rem] overflow-hidden animate-moving-background' >
+        
       </div>
   )
 }
