d7101f728127d46b6d1b45a621fd0b88f871a5e1
bbbaipooo <baipoo_B@hotmail.com>
2023-10-12 15:14:55 +0700

    home page update

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,43 +1,44 @@
 import { useState } from 'react'
 import React from 'react'
 import { background, island, button, buttonM, sprite, chicken1, chicken2 } from "../../assets/home"
+import Flower from "../../components/Sprite/Flower"
 
 function Home() {
   const [count, setCount] = useState(0)
 
   return (
     <div style={{ backgroundImage: `url(${background})` }} className='w-full h-[100vh] flex justify-center items-center gap-[5rem] overflow-hidden animate-moving-background relative' >
-      <div className="absolute z-10 top-[5.5rem] left-[4rem] leading-[10rem] mainTitle" >
-        <div className='text-4xl text-[#6A8139] subTitle'>WHEEL</div>
-        <div className='text-[12.6rem] text-[#A4C263] tracking-tight'>ROBOT</div>
-        <div className='text-[6.4rem] text-[#A4C263] submainTitle leading-[8rem]   tracking-tighter'>SIMULATION</div>
-        <div className='text-4xl text-[#6A8139] subTitle font-extrabold leading-[2rem] '>WITH CUSTOMIZABLE PATHING</div>
+      <div className="absolute z-10 top-[3.5rem] left-[4rem] leading-[10rem] drop-shadow-2xl" >
+        <div className='text-4xl text-[#6A8139] font-outline-2 '>WHEEL</div>
+        <div className='text-[11.6rem] text-[#A4C263] font-outline-4 tracking-tight'>ROBOT</div>
+        <div className='text-[5rem] text-[#A4C263] font-outline-4 leading-[8rem] tracking-widest'>SIMULATION</div>
+        <div className='text-4xl text-[#6A8139] font-outline-2  leading-[2rem] '>WITH CUSTOMIZABLE PATHING</div>
       </div>
       <div className='absolute z-0'>
         <img src={island} className='w-screen pr-[100px] ' />
       </div>
-      <div className='absolute z-10 top-[46rem] left-[5.5rem]'>
-        <img src={buttonM} className='w-[33rem] pr-[100px]' />
+      <div className='absolute z-10 top-[40rem] left-[5.5rem]'>
+        <img src={buttonM} className='w-[30rem] pr-[90px]' />
         <div className='flex '>
-          <img src={button} className='w-[13.5rem]' />
-          <img src={button} className='w-[13.5rem]' />
+          <img src={button} className='w-[12.3rem]' />
+          <img src={button} className='w-[12.3rem]' />
         </div>
       </div>
-      <div className='absolute z-10 top-[46rem] left-[5.5rem]'>
-        <div className='w-[34rem] pr-[110px] pt-[1.5rem]  text-center text-8xl text-[#90625D]' >PLAY</div>
-        <div className='flex mt-[4.9rem]'>
-          <div className='ml-[2rem] text-5xl text-[#461E19]'>EXIT</div>
-          <div className='ml-[4.6rem] text-5xl text-[#461E19]'>HOW</div>
+      <div className='absolute z-10 top-[40rem] left-[5.5rem]'>
+        <div className='w-[34rem] pr-[140px] pt-[1.5rem]  text-center text-7xl text-[#90625D]' >PLAY</div>
+        <div className='flex mt-[5.3rem]'>
+          <div className='ml-[2.6rem] text-4xl text-[#461E19]'>EXIT</div>
+          <div className='ml-[5.6rem] text-4xl text-[#461E19]'>HOW</div>
         </div>
       </div>
       {/* <div className='relative z-10'> */}
-          <div className='absolute z-10 top-[23rem] right-[27.5rem]'>
-            <img src={sprite} />
+          <div className='absolute z-10 top-[20rem] right-[27.5rem] '>
+            <Flower/>
           </div>
-          <div className='absolute z-10 top-[48rem] right-[54.5rem]'>
+          <div className='absolute z-10 top-[43rem] right-[52.3rem] animate-bounce'>
             <img src={chicken1} />
           </div>
-          <div className='absolute z-10 top-[46rem] left-[36rem]'>
+          <div className='absolute z-10 top-[41rem] left-[34rem] animate-bounce'>
             <img src={chicken2} />
           </div>
         </div>
