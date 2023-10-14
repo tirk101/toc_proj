b8a9ed31a8f0f195569be0d941ce825463241b2e
Natdanai <88823702+corodimas@users.noreply.github.com>
2023-10-14 21:03:56 +0700

    refactor

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import React from 'react'
 import { background, island, button, buttonM, sprite, chicken1, chicken2 } from "../../assets/home"
 import Flower from "../../components/Sprite/Flower"
-
+import { useNavigate} from 'react-router-dom'
 function Home() {
   const [count, setCount] = useState(0)
-
+  const navigate = useNavigate()
   return (
       <div style={{ backgroundImage: `url(${background})` }} className='w-full h-[100vh] flex justify-center items-center gap-[5rem] overflow-hidden animate-moving-background relative' >
         <div className="absolute z-10 top-[3.5rem] left-[4rem] leading-[10rem] drop-shadow-2xl select-none" >
@@ -29,7 +29,7 @@ function Home() {
             <img src={island} className=' pr-[100px] z-10' />
         </div>
         <div className='absolute z-10 top-[40rem] left-[5.5rem] select-none'>
-          <div className=' hover:scale-110 ml-1 cursor-pointer'>
+          <div className=' hover:scale-110 ml-1 cursor-pointer' onClick={()=>{navigate('playground')}}>
             <img src={buttonM} className='w-[30rem] pr-[90px]' />
             <p className='w-[34rem] pr-[140px] pt-[1.5rem] mt-[-9rem] text-center text-7xl text-[#90625D]' >PLAY</p>
           </div>
@@ -40,7 +40,7 @@ function Home() {
             </div>
             <div className='hover:scale-110 cursor-pointer'>
               <img src={button} className='w-[12.3rem] mt-[2.5rem]' />
-              <p className='ml-[3.5rem] text-4xl mt-[-5.5rem] text-[#461E19]'>HOW</p>
+              <p className='ml-[3.5rem] text-4xl mt-[-5.5rem] text-[#461E19]' onClick={()=>{navigate('tutorial')}}>HOW</p>
             </div>
           </div>
         </div>
