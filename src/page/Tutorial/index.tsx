c76f1f9768642030cbcf3eef99a3141a650c3347
NonGP33M <phumrapee.sk@gmail.com>
2023-10-16 02:25:18 +0700

    Update : Tutorial Page

--- src/page/Tutorial/index.tsx
+++ src/page/Tutorial/index.tsx
@@ -25,17 +25,17 @@ const Index = () => {
           style={{ backgroundImage: `url(${background})` }}
           className="w-full h-[100vh] flex justify-center items-center gap-[5rem] overflow-hidden animate-moving-background absolute"
         ></div>
-        <div className="absolute bottom-0 w-[50%] h-screen">
-          <img src={label} alt="label" className="" />
+        <div className="absolute bottom-[8vw] w-[50%]">
+          <img src={label} alt="label" className="w-full" />
         </div>
         <div className="absolute bottom-0 w-full">
-          <img src={ground} alt="ground" className="" />
+          <img src={ground} alt="ground" className="w-full" />
         </div>
         <div className="absolute flex justify-between w-full px-5 py-5 bottom-0">
           <div>
             <img
               src={leftArrow}
-              className="pointer-events-auto hover:translate-y-[-3px] duration-100 active:opacity-70 active:hover:translate-y-[3px]"
+              className="pointer-events-auto hover:translate-y-[-3px] duration-100 active:opacity-70 active:hover:translate-y-[3px] w-[10vh]"
               draggable={false}
               onClick={() => {
                 navigate("/");
@@ -45,7 +45,7 @@ const Index = () => {
           <div>
             <img
               src={rightArrow}
-              className="pointer-events-auto hover:translate-y-[-3px] duration-100 active:opacity-70 active:hover:translate-y-[3px]"
+              className="pointer-events-auto hover:translate-y-[-3px] duration-100 active:opacity-70 active:hover:translate-y-[3px] w-[10vh]"
               draggable={false}
               onClick={() => {
                 navigate("/tutorial2");
@@ -54,7 +54,7 @@ const Index = () => {
           </div>
         </div>
         <div className="absolute top-10 left-8 z-0">
-          <img src={HOW} alt="how" className="w-[90vh]" />
+          <img src={HOW} alt="how" className="w-[45vw]" />
           <div className="absolute animate-bounce left-2/4 top-1/3">
             <img src={cat} alt="cat" className="w-[70%]" />
           </div>
