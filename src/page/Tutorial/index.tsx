354799ade2404b09ad3d5229b0d183bf91907db3
Boonyawee Kreesaeng <64010459@kmitl.ac.th>
2023-10-16 21:05:51 +0700

    update tutorial page

--- src/page/Tutorial/index.tsx
+++ src/page/Tutorial/index.tsx
@@ -24,7 +24,9 @@ const Index = () => {
         <div
           style={{ backgroundImage: `url(${background})` }}
           className="w-full h-[100vh] flex justify-center items-center gap-[5rem] overflow-hidden animate-moving-background absolute"
-        ></div>
+        >
+        </div>
+        <div className="absolute right-5 text-[#ffffff] text-2xl">1/2</div>
         <div className="absolute bottom-[8vw]">
           <img src={label} alt="label" className="h-screen" />
         </div>
@@ -53,27 +55,6 @@ const Index = () => {
             />
           </div>
         </div>
-        <div className="absolute top-10 left-8 z-0">
-          <img src={HOW} alt="how" className="w-[45vw]" />
-          {/* <div className="absolute animate-bounce left-2/4 top-1/3">
-            <img src={cat} alt="cat" className="w-[70%]" />
-          </div> */}
-        </div>
-
-        {/* <div className="flex flex-col justify-center items-center font-bold z-40">
-          <button
-            onClick={handleTranslateUp}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          >
-            Translate Up
-          </button>
-          <div
-            className="transition-transform transform translate-y-[-10px]"
-            style={{ transform: `translateY(${translateY}px)` }}
-          >
-            <Flower />
-          </div>
-        </div> */}
       </div>
     </div>
   );
--- src/page/Tutorial2/index.tsx
+++ src/page/Tutorial2/index.tsx
@@ -7,7 +7,7 @@ import {
   leftArrow,
   rightArrow,
   cat,
-  ground
+  ground,
 } from "../../assets/tutorial";
 import { useNavigate } from "react-router-dom";
 const Index = () => {
@@ -24,18 +24,20 @@ const Index = () => {
         <div
           style={{ backgroundImage: `url(${background})` }}
           className="w-full h-[100vh] flex justify-center items-center gap-[5rem] overflow-hidden animate-moving-background absolute"
-        ></div>
-        <div className="absolute bottom-0 ">
-          <img src={label} alt="label" />
+        >
+        </div>
+        <div className="absolute right-5 text-[#ffffff] text-2xl">2/2</div>
+        <div className="absolute bottom-[8vw]">
+          <img src={label} alt="label" className="h-screen" />
         </div>
         <div className="absolute bottom-0 w-full">
           <img src={ground} alt="ground" className="w-full" />
         </div>
-        <div className="absolute flex  w-full px-5 py-5 bottom-0">
+        <div className="absolute flex justify-between w-full px-5 py-5 bottom-0">
           <div>
             <img
               src={leftArrow}
-              className="pointer-events-auto hover:translate-y-[-3px] duration-100 active:opacity-70 active:hover:translate-y-[3px]"
+              className="pointer-events-auto hover:translate-y-[-3px] duration-100 active:opacity-70 active:hover:translate-y-[3px] w-[10vh]"
               draggable={false}
               onClick={() => {
                 navigate("/tutorial");
@@ -43,27 +45,6 @@ const Index = () => {
             />
           </div>
         </div>
-        <div className="absolute top-10 left-8 z-0">
-          <img src={HOW} alt="how" className="w-[90vh]" />
-          <div className="absolute animate-bounce left-2/4 top-1/3">
-            <img src={cat} alt="cat" className="w-[70%]" />
-          </div>
-        </div>
-
-        {/* <div className="flex flex-col justify-center items-center font-bold z-40">
-          <button
-            onClick={handleTranslateUp}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          >
-            Translate Up
-          </button>
-          <div
-            className="transition-transform transform translate-y-[-10px]"
-            style={{ transform: `translateY(${translateY}px)` }}
-          >
-            <Flower />
-          </div>
-        </div> */}
       </div>
     </div>
   );
